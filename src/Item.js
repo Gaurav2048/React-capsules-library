783ba380798dff9dfe60ed8e6d9a76baa3f42d46
Gaurav2048 <gunjankalita836@gmail.com>
2020-09-22 14:11:02 +0530

    Minor bug fix

---
+++ src/Item.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+const Item = ({
+  item,
+  style,
+  onClick,
+  onDragEnter,
+  icon,
+  onDragStart,
+  onDragEnd,
+  position,
+}) => {
+  return (
+    <div
+      onClick={(e) => onClick(e)}
+      style={{
+        ...style,
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '5px',
+        alignItems: 'center',
+      }}
+      onDragStart={(e) => onDragStart(e, position)}
+      onDragEnter={(e) => onDragEnter(e, position)}
+      onDragEnd={(e) => onDragEnd(e, position)}
+      onDragOver={(e) => e.preventDefault()}
+      draggable
+    >
+      <div style={{ display: 'inline' }}> {item} </div>
+      <img
+        src={icon}
+        alt="icon"
+        style={{
+          width: '10px',
+          height: '10px',
+          backgroundColor: '#DDDDDD',
+          padding: '5dp',
+          cursor: 'pointer',
+          marginLeft: '10px',
+        }}
+      />
+    </div>
+  );
+};
+
+export default Item;
