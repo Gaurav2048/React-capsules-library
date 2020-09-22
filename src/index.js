13ab870d25f3bf6977522aefb4d1f1328562d7f5
Gaurav2048 <gunjankalita836@gmail.com>
2020-09-22 14:10:32 +0530

    update implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,60 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useRef } from 'react';
+import Item from './Item';
+import TextInput from './TextInput';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const ReactBulletDnd = ({ children, style, data, set }) => {
+  const draggingItem = useRef(null);
+  const draggingItemIndex = useRef(null);
+  const draggEnterIndex = useRef(null);
+
+  const onClick = (e) => {
+  };
+
+  const onDragStart = (e, index) => {
+    draggingItem.current = data[index];
+    draggingItemIndex.current = index;
+  };
+
+  const onDragEnter = (e, index) => {
+    if (draggingItemIndex.current === index) return;
+    if (index === null || draggEnterIndex.current === index) return;
+    draggEnterIndex.current = index;
+    const ItemCopy = JSON.parse(JSON.stringify(data));
+    ItemCopy.splice(draggingItemIndex.current, 1);
+    ItemCopy.splice(index, 0, draggingItem.current);
+    draggingItemIndex.current = index;
+    set(ItemCopy);
+  };
+
+  const onDragEnd = (e, index) => {
+    draggingItem.current = null;
+    draggingItemIndex.current = null;
+    draggEnterIndex.current = null;
+  };
+
+  return (
+    <div style={{ ...style }}>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        {React.Children.map(children, (child) => {
+          if (child.type.name !== 'TextInput') {
+            return React.cloneElement(child, {
+              onClick,
+              onDragEnter,
+              onDragStart,
+              onDragEnd,
+            });
+          } else {
+            return React.cloneElement(child, {
+              addItem: child.props.addItem,
+            });
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+ReactBulletDnd.Item = Item;
+ReactBulletDnd.TextInput = TextInput;
+
+export default ReactBulletDnd;
